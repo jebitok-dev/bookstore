ea1fd304836c6c9ea05b4b197e9fb0731a0d14f3
Jebitok <sharonkosgei4@gmail.com>
2021-06-21 13:37:42 +0300

    Add Changes to Index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Books from './reducers/Books';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(Books);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
