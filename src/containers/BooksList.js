9901c3c2c30ce6ccbec93b03c452fb5bd10a55b2
Jebitok <sharonkosgei4@gmail.com>
2021-06-21 12:55:54 +0300

    Add Booklist Container

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
 
-const BooksList = () => (
+const BooksList = ({ books }) => (
   <div>
-    <h1>BooksList</h1>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      {books.map((book) => (
+        /* eslint-disable */
+        <Book key={book.id} {...book} />
+      ))}
+    </table>
   </div>
 );
+/* eslint-enable */
 
-export default BooksList;
+const mapStateToProps = (state) => ({
+  books: state.books,
+});
+
+BooksList.propTypes = {
+  books: PropTypes.objectOf,
+};
+
+BooksList.defaultProps = {
+  books: {},
+};
+
+export default connect(mapStateToProps)(BooksList);
