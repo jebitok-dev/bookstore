77b4930bfd1224e493e27beb1dd04fe61fd20837
Jebitok <sharonkosgei4@gmail.com>
2021-06-22 16:49:58 +0300

    Add Changes to Form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,55 +1,78 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addBook } from '../actions/index';
 
-const BooksForm = () => {
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const ref = useRef(null);
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
 
-  const handleChange = (e) => {
-    const { title, category } = e.target;
-  }  
+    this.ref = React.createRef();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
+  }
 
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const renderCategories = categories.map((item) => (
-    <option key={uuidv4()}>{item}</option>
-  ));
+  handleDrop() {
+    this.setState({ title: this.inputNode.value, category: this.myRef.current.value });
+  }
 
-  return (
-    <div>
-      <button type="button" className="addBook btn btn-primary" data-toogle="modal" data-target="#exampleModal">
-        Add a book!
-      </button>
-      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Add to Bookstore</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <div className="dropdown">
-                <select className="custom-select" id="inputGroupSelect01">
-                  <option selected>Choose Category</option>
-                  {renderCategories}
-                </select>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" />
+  handleChange(e) {
+    e.preventDefault();
+    this.props.addBook(this.state);
+  }
+
+  render() {
+    const stringId = 'category';
+    const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    const renderCategories = categories.map((item, id) => (
+      <option key={uuidv4()} id={stringId + id.toString()} value={item}>{item}</option>
+    ));
+
+    return (
+      <div>
+        <button type="button" className="addBook btn btn-primary" data-toogle="modal" data-target="#exampleModal">
+          Add a book!
+        </button>
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Add to Bookstore</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Add a book</button>
+              <div className="modal-body">
+                <form id="bookForm" name="updateTaskForm">
+                  <div className="title">
+                    <label htmlFor="fname">Title</label>
+                    <br />
+                    <input type="text" id="fname" name="title" ref={(node) => (this.inputNode = node)} />
+                    <br />
+                  </div>
+                  <div className="dropdown">
+                    <select className="custom-select" id="inputGroupSelect01" onChange={this.handleDrop} ref={this.myRef} value={this.state.selectValue}>
+                      <option selected>Choose Category</option>
+                      {renderCategories}
+                    </select>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary" onClick={this.handleChange}>Add a book</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default BooksForm;
