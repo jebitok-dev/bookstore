12cc36db00b80a1c4d6d6d57ba8ef25a6b362854
Jebitok <sharonkosgei4@gmail.com>
2021-06-23 11:00:24 +0300

    Add Changes to Book Component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-const Book = () => {
-  const { id, title, category } = useState('');
+const Book = (book) => {
+  const {
+    id, title, category, handleRemoveBook,
+  } = book;
   return (
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="button" className="btn btn-primary" onClick={() => handleRemoveBook(book)}>Remove</button></td>
     </tr>
   );
-/* eslint-disable */
-
-  Book.propTypes = {
-    id: PropTypes.number,
-    title: PropTypes.string,
-    category: PropTypes.string,
-  };
-
-  Book.defaultProps = {
-    id: '',
-    title: '',
-    category: '',
-  };
 };
-/* eslint-enable */
 
 export default Book;
