656b9689c744536ef9c0478e2ad304c041ca9a4b
Jebitok <sharonkosgei4@gmail.com>
2021-06-25 16:37:08 +0300

    Add Changes to Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
+import Navbar from './Navbar';
 
 const App = () => (
-  <div className="App">
-    <BooksForm />
-    <BooksList />
+  <div>
+    <Navbar />
+    <div className="App">
+      <BooksForm />
+      <BooksList />
+    </div>
   </div>
 );
 
