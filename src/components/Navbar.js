7aa538853bb924567f63211a67d5f7f8f241fedc
Jebitok <sharonkosgei4@gmail.com>
2021-06-25 13:05:49 +0300

    Fix Linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 const Navbar = () => (
-    <div class="navbar font-montserrat">
-        <div className="navbar-links">
-            <h1 className="title">Bookstore CMS</h1>
-            <a className="navbar-link selected" href="/index.html">
-                BOOKS
-            </a>
-            <a class="navbar-link" href="/index.html">
-                CATEGORIES
-            </a>
-        </div>
+  <div className="navbar font-montserrat">
+    <div className="navbar-links">
+      <h1 className="title">Bookstore CMS</h1>
+      <a className="navbar-link selected" href="/index.html">
+        BOOKS
+      </a>
+      <a className="navbar-link" href="/index.html">
+        CATEGORIES
+      </a>
     </div>
+  </div>
 );
 
 export default Navbar;
