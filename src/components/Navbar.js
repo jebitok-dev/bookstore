fcaf73cc0bd2a5abbff8f7237a0d1f8055b434cd
Jebitok <sharonkosgei4@gmail.com>
2021-06-25 12:59:09 +0300

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => (
+    <div class="navbar font-montserrat">
+        <div className="navbar-links">
+            <h1 className="title">Bookstore CMS</h1>
+            <a className="navbar-link selected" href="/index.html">
+                BOOKS
+            </a>
+            <a class="navbar-link" href="/index.html">
+                CATEGORIES
+            </a>
+        </div>
+    </div>
+);
+
+export default Navbar;
