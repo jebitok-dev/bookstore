b1bba3792e9b36b86e96a1534ced86d43c11e812
jebitok-dev <sharonjebitok@gmail.com>
2021-06-30 11:57:49 +0300

    Add Changes to Book Component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import circle from '../images/circle.png';
+/* eslint-disable react/prop-types */
+const Book = ({
+  id, title, category, handleClick,
+}) => (
+  <div className="book-item roboto">
+    <div className="book-data">
+      <p className="category mont">{category}</p>
+      <p className="title mont">{title}</p>
+      <p className="author mont">Author Name</p>
+      <div className="links">
+        <button type="button" className="link">
+          Comments
+        </button>
+        <span className="span" />
+        <button
+          className="link"
+          type="button"
+          onClick={() => {
+            handleClick({ id, title, category });
+          }}
+        >
+          Remove
+        </button>
+        <span className="span" />
+        <button type="button" className="link">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="reading-progress">
+      <img className="oval" alt="circle" src={circle} />
+      <div>
+        <p className="percent-complete font-montserrat">
+          {Math.floor(Math.random() * 100)}
+          %
+        </p>
+        <p className="completed font-montserrat">Completed</p>
+      </div>
+    </div>
+    <div className="line-2" />
+    <div className="chapter-info">
+      <p className="current-chapter font-robotoslab">CURRENT CHAPTER</p>
+      <p className="chapter font-robotoslab">
+        Chapter
+        {Math.floor(Math.random() * 20)}
+      </p>
+      <button type="button" className="update-progress font-robotoslab">
+        UPDATE CHAPTER
+      </button>
+    </div>
+  </div>
+);
 
-const Book = (book) => {
-  const {
-    id, title, category, handleRemoveBook,
-  } = book;
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td><button type="button" className="btn btn-primary" onClick={() => handleRemoveBook(book)}>Remove</button></td>
-    </tr>
-  );
+Book.propTypes = {
+  handleClick: PropTypes.func,
+  book: PropTypes.shape({
+    bookId: PropTypes.number,
+    title: PropTypes.string,
+    category: PropTypes.string,
+  }),
+};
+Book.defaultProps = {
+  book: null,
+  handleClick: null,
 };
 
 export default Book;
